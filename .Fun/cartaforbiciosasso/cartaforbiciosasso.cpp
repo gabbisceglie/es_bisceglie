1c555ed4bb07d7b3a94fa88879d6b19bb68c5051
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-03-12 22:53:03 +0100

    fun

--- .Fun/cartaforbiciosasso/cartaforbiciosasso.cpp
+++ .Fun/cartaforbiciosasso/cartaforbiciosasso.cpp
@@ -5,73 +5,96 @@ using namespace std;
 int main() {
     int i; //inserito
     int mossa; //mossa casuale del pc
+    int p1=0, p2=0, round=0; //punteggio
     string mossa2;
 
-//genera un opzione casuale tra 1 e 3
-srand(time(0));
-mossa = rand() % 3 + 1;
-
-if(mossa==1) {
-    mossa2 = "Sasso";
-} else if(mossa==2) {
-    mossa2 = "Carta";
-} else if(mossa==3) {
-    mossa2 = "Forbici";
-}
+    //genera un opzione casuale tra 1 e 3
+    srand(time(0));
 
 do {
-cout << "\n Inserisci '1' per iniziare la partita: ";
-cin >> (i);
-} while (i!=1); {
+    cout << "\nLa partita dura 10 round, buona fortuna! \nInserisci " << "1" << " per iniziare la partita: ";
+    cin >> i;
+} while (i != 1);
 
-}
-if (i==1) {
-    cout << "\n1)Sasso\n2)Carta\n3)Forbici\nSeleziona un opzione:";
-}
-cin >> i;
+
+for (round = 0; round < 10; round++) {
+    mossa = rand() % 3 + 1;
+
+    if (mossa == 1) {
+        mossa2 = "Sasso";
+    } else if (mossa == 2) {
+        mossa2 = "Carta";
+    } else if (mossa == 3) {
+        mossa2 = "Forbici";
+    }
+
+    cout << "\n1) Sasso\n2) Carta\n3) Forbici\nSeleziona un opzione:";
+    cin >> i;
+
 switch (i) {
     case 1:
-    cout << "Hai scelto 'Sasso'";
-    if(mossa==1) {
+        cout << "Hai scelto 'Sasso' \n---------------------------------------------------------";
+    if (mossa == 1) {
         cout << "\nPareggio!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if (mossa==2) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        p2++;
+    } else if (mossa == 2) {
         cout << "\nHai perso!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if (mossa==3) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p2++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------"; 
+    } else if (mossa == 3) {
         cout << "\nHai vinto!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------";
     }
     break;
-    //____________________________________
+//____________________________________
     case 2:
-    cout << "Hai scelto 'Carta'";
-    if(mossa==2) {
+        cout << "Hai scelto 'Carta'\n---------------------------------------------------------";
+    if (mossa == 2) {
         cout << "\nPareggio!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if(mossa==3) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        p2++;
+    } else if (mossa == 3) {
         cout << "\nHai perso!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if(mossa==1) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p2++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------";
+    } else if (mossa == 1) {
         cout << "\nHai vinto!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------";
     }
     break;
-    //____________________________________
+//____________________________________
     case 3:
-    cout << "\nHai scelto 'Forbici'";
-    if (mossa==3) {
+        cout << "\nHai scelto 'Forbici'\n---------------------------------------------------------";
+    if (mossa == 3) {
         cout << "\nPareggio!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if(mossa==2) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        p2++;
+    } else if (mossa == 2) {
         cout << "\nHai vinto!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
-    } else if(mossa==1) {
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p1++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------";
+    } else if (mossa == 1) {
         cout << "\nHai perso!";
-        cout << "\n Il tuo avversario aveva scelto: " << mossa2;
+        cout << "\nIl tuo avversario aveva scelto: " << mossa2;
+        p2++;
+        cout << "\nPunteggio: Utente: " << p1 << " Computer: " << p2 << "\n---------------------------------------------------------";
     }
     break;
     }
+}
+    cout << "\n\tPARTITA TERMINATA!!";
+    cout << "\n\nIl tuo punteggio: " << p1 << "\nIl punteggio del tuo avversario: " << p2;
 
     return 0;
 }

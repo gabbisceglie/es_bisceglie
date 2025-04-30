51b287a24c6d6aa4320a3c56cdc3245cf40add7d
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-04-30 19:37:37 +0200

    commit es28

---
+++ C++/es28/es28.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Funzione che calcola il quadrato di un numero
+    void calcolo(int n) {
+        cout << "Il quadrato di " << n << " e': " << n * n << endl;
+    }
+
+
+int main() {
+
+    int n;
+
+    //Richiesta di un numero intero all'utente
+    cout << "Inserisci un numero intero: ";
+    cin >> n;
+
+    //Richiamo la funzione per calcolare il quadrato
+    calcolo(n);
+
+    return 0;
+}
